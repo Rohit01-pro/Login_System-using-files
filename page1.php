3a201e2d862866f7e155a5a5dd463c50990fe204
Rampal25 <70803282+Rampal25@users.noreply.github.com>
2020-09-30 11:18:22 -0700

    use <article> tag

--- page1.php
+++ page1.php
@@ -16,7 +16,7 @@
     <div class="container">
         <h1>Page 1</h1>
         <p>This is Page 1.</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nostrum, accusamus magni maxime mollitia, alias exercitationem vitae beatae eos provident aliquid ut deleniti sit quaerat voluptatibus! Voluptas neque totam quas?</p>
+        <article>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error nostrum, accusamus magni maxime mollitia, alias exercitationem vitae beatae eos provident aliquid ut deleniti sit quaerat voluptatibus! Voluptas neque totam quas?</article>
     </div>
 </body>
 </html>
